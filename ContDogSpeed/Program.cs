6ecc972202794795cafdebdd46e8757d41f8f063
Sergey <gusevsergey85@gmail.com>
2022-07-08 13:50:17 +0300

    count dog times

---
+++ ContDogSpeed/Program.cs
@@ -0,0 +1,19 @@
+﻿int count = 0, distance = 10000, time,  firstFriendSpeed = 1, seconsFriendSpeed = 2, dogSpeed = 5, friend = 2;
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (seconsFriendSpeed + dogSpeed);
+        friend = 1; 
+    }
+    distance = distance - (firstFriendSpeed + seconsFriendSpeed) * time;
+    count++;
+
+} 
+
+Console.Write($"Собака пробежит {count} раз");
--- Example006max/Program.cs
+++ Example006max/Program.cs
@@ -6,30 +6,13 @@ int num5 = 40;
 int num6 = 31;
 int num7 = 69;
 int max = num1; 
-if (num2 > max)
-{
-    max = num2;
-}
-if (num3 > max)
-{
-    max = num3;
-}
-if (num4 > max)
-{
-    max = num4;
-}
-if (num5 > max)
-{
-    max = num5;
-}
-if (num6 > max)
-{
-    max = num6;
-}
-if (num7 > max)
-{
-    max = num7;
-}
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+if (num4 > max) max = num4;
+if (num5 > max) max = num5;
+if (num6 > max) max = num6;
+if (num7 > max) max = num7;
+
 Console.WriteLine(max);
 
 
