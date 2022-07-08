efc597e77d973f9e8a7e89e0dc2ec8b9e3a92070
Sergey <gusevsergey85@gmail.com>
2022-07-08 13:10:15 +0300

    Finding max

---
+++ Example006max/Program.cs
@@ -0,0 +1,35 @@
+﻿int num1 = 3;
+int num2 = 10;
+int num3 = -96;
+int num4 = 5;
+int num5 = 40;
+int num6 = 31;
+int num7 = 69;
+int max = num1; 
+if (num2 > max)
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+if (num4 > max)
+{
+    max = num4;
+}
+if (num5 > max)
+{
+    max = num5;
+}
+if (num6 > max)
+{
+    max = num6;
+}
+if (num7 > max)
+{
+    max = num7;
+}
+Console.WriteLine(max);
+
+
